9dc5540f24405a7ecf5bba33d9d77b04de278f16
RicardoEspiritu <95498222+Richard5102@users.noreply.github.com>
2024-01-24 09:35:17 -0600

    Crear metodos separados

--- src/main/java/UtilsQueueDeclaration.java
+++ src/main/java/UtilsQueueDeclaration.java
@@ -1,5 +1,8 @@
 import com.rabbitmq.client.*;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class UtilsQueueDeclaration {
 
     public static void main(String[] argv) throws Exception {
@@ -14,9 +17,71 @@ public class UtilsQueueDeclaration {
             channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT); // CREAR EXCHANGE DE TIPO FANOUT
             channel.queueDeclare(QUEUE_NAME, false, false, false, null); // CREAR QUEUE DEFAULT
             channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ""); // ENLAZAR EXCHANGE CON QUEUE
-            // PARA LIMPIAR UNA COLA NO HAY UN CÓDIGO EN ESPECÍFICO, SE DEBE DE ELIMINAR
-            channel.queueDelete(QUEUE_NAME);
-            channel.exchangeDelete(EXCHANGE_NAME);
+            channel.queueDelete(QUEUE_NAME); // BORRAR QUEUE
+            channel.exchangeDelete(EXCHANGE_NAME); // BORRAR EXCHANGE
+            channel.queuePurge(QUEUE_NAME); // LIMPIAR QUEUE
+        }
+        crearExchange(EXCHANGE_NAME);
+        declararQUEUE(QUEUE_NAME);
+        unirEXCHANGEQUEUE(EXCHANGE_NAME, QUEUE_NAME);
+        borrarQUEUE(QUEUE_NAME);
+        borrarEXCHANGE(EXCHANGE_NAME);
+        limpiarQUEUE(QUEUE_NAME);
+    }
+
+    public static void crearExchange(String EXCHANGE_NAME) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+             channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT); // CREAR EXCHANGE DE TIPO FANOUT
+
         }
     }
+
+    public static void declararQUEUE(String QUEUE_NAME) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+               channel.queueDeclare(QUEUE_NAME, false, false, false, null); // CREAR QUEUE DEFAULT
+        }
+    }
+
+    public static void  unirEXCHANGEQUEUE(String EXCHANGE_NAME, String QUEUE_NAME) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ""); // ENLAZAR EXCHANGE CON QUEUE
+        }
+    }
+
+    public static void  borrarQUEUE( String QUEUE_NAME) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+             channel.queueDelete(QUEUE_NAME); // BORRAR QUEUE
+        }
+    }
+
+    public static void  borrarEXCHANGE( String EXCHANGE_NAME) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDelete(EXCHANGE_NAME); // BORRAR EXCHANGE
+        }
+    }
+
+    public static void  limpiarQUEUE( String QUEUE_NAME) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queuePurge(QUEUE_NAME); // LIMPIAR QUEUE
+        }
+    }
+
 }
